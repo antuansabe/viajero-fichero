eb00dc6ef0fc55d14d3a49db17523ef0a3c43151
Antonio Dromundo <Antonn@MacBook-Pro-666.local>
2024-07-16 22:38:40 -0600

    modificacion de la pagina de contacto

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,16 @@ import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import rehypePluginImageNativeLazyLoading from 'rehype-plugin-image-native-lazy-loading';
 
 import { remarkReadingTime } from "./src/utils/all";
+
 
 export default defineConfig({
   site: "https://stablo-astro.web3templates.com",
   markdown: {
     remarkPlugins: [remarkReadingTime],
-    rehypePlugins: ["rehype-plugin-image-native-lazy-loading"],
+    rehypePlugins: [rehypePluginImageNativeLazyLoading],
     extendDefaultPlugins: true,
   },
   integrations: [
