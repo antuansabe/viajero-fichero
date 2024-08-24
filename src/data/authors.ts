a8df0fc955e4f4da0b15c09577ba969ddaecd424
Antonio Dromundo <antuansabe@gmail.com>
2024-08-24 09:06:05 +0200

    cambio en los archivos de fotos y actualizacion de foto de autor

--- src/data/authors.ts
+++ src/data/authors.ts
@@ -11,7 +11,7 @@ export const authors: Props[] = [
   {
     name: "Antonio Dromundo",
     slug: "antonio-dromundo",
-    image: "./src/assets/authors/mario.webp",
+    image: "./src/assets/authors/antonio.jpg",
     bio: "Soy un desarrollador de software apasionado por viajar, explorar nuevos destinos y capturar momentos a través de la fotografía. Cuando no estoy escribiendo código, estoy recorriendo el mundo, reflexionando sobre mis experiencias y encontrando inspiración en cada rincón. En “Viajero Fachero”, combino mis dos pasiones, compartiendo mis proyectos, reflexiones y fotos que narran las historias de mis aventuras globales.",
   }
   
