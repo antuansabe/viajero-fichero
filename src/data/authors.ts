31f9198d4eec3c2fcdb49e86f8cda5bea8c37365
Antonio Dromundo <antuansabe@gmail.com>
2024-09-02 16:57:15 -0600

    arreglando errores de eliminacion de autores

--- src/data/authors.ts
+++ src/data/authors.ts
@@ -13,6 +13,10 @@ export const authors: Props[] = [
     slug: "antonio-dromundo",
     image: "./src/assets/authors/antonio.jpg",
     bio: "Soy un desarrollador de software apasionado por viajar, explorar nuevos destinos y capturar momentos a través de la fotografía. Cuando no estoy escribiendo código, estoy recorriendo el mundo, reflexionando sobre mis experiencias y encontrando inspiración en cada rincón. En “Viajero Fachero”, combino mis dos pasiones, compartiendo mis proyectos, reflexiones y fotos que narran las historias de mis aventuras globales.",
-  }
-  
+  },
+ 
 ];
+
+
+
+
